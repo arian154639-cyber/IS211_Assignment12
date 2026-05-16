d30926d0215bb2f3cb32c59796902f69952b9481
arian154639-cyber <arian154639@gmail.com>
2026-05-15 23:21:04 -0400

    Add files via upload

--- hw12db.py
+++ hw12db.py
@@ -9,17 +9,17 @@ with open("schema.sql", "r") as schema_file:
     cursor.execute("""
     INSERT INTO students (student_id, student_first_name, student_last_name)
     VALUES (1, "John", "Smith")
-    """);
+    """)
 
     cursor.execute("""
     INSERT INTO quizzes (quiz_id, quiz_subject, quiz_question_count, quiz_date)
     VALUES (1, "Python Basics", 5, "February, 5th, 2015")
-    """);
+    """)
 
     cursor.execute("""
     INSERT INTO results (student_id, quiz_id, quiz_score)
     VALUES (1, 1, 85)
-    """);
+    """)
 
 connection.commit()
 connection.close()
